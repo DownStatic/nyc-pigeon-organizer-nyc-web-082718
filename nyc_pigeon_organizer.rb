66168e1226f091f80e981f4e59781c4a55a6ebb1
Christopher Stull <chris.stull@gmail.com>
2018-08-17 16:11:43 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,32 +1,32 @@
-def nyc_pigeon_organizer(data)
-  new_pigeon_data = {}
-  names = []
- data.each do |cgl,pigeon_info|
-    pigeon_info.each do |key,names_array|
-      names_array.each do |name|
-        if not names.include?(name)
-          names << name
+  def nyc_pigeon_organizer(data)
+    new_pigeon_data = {}
+    names = []
+   data.each do |cgl,pigeon_info|
+      pigeon_info.each do |key,names_array|
+        names_array.each do |name|
+          if not names.include?(name)
+            names << name
+          end
         end
       end
     end
-  end
-  
-  names.each do |name|
-    new_pigeon_data[name] = {
-      :color => [],
-      :gender => [],
-      :lives => []
-    }
-  end
-  
-  new_pigeon_data.each do |name,info|
-    data.each do |cgl,pigeon_info|
-      pigeon_info.each do |stat,pigeons|
-        if pigeons.include?(name)
-          info[cgl] << stat.to_s
+    
+    names.each do |name|
+      new_pigeon_data[name] = {
+        :color => [],
+        :gender => [],
+        :lives => []
+      }
+    end
+    
+    new_pigeon_data.each do |name,info|
+      data.each do |cgl,pigeon_info|
+        pigeon_info.each do |stat,pigeons|
+          if pigeons.include?(name)
+            info[cgl] << stat.to_s
+          end
         end
       end
     end
+    return new_pigeon_data
   end
-  return new_pigeon_data
-end
